e58cab3ebe5a0e063ebf75816969acdea17cc495
Dmytro <dmtlvn@gmail.com>
2024-08-06 18:38:26 +0200

    foo

--- deformable_modules.py
+++ deformable_modules.py
@@ -52,13 +52,13 @@ class DeformableTransformerEncoder(nn.Module):
     @staticmethod
     def get_reference_points(spatial_shapes, valid_ratios, device):
         reference_points_list = []
-        for lvl, (H_, W_) in enumerate(spatial_shapes):
+        for lvl, shape in enumerate(spatial_shapes):
             ref_y, ref_x = torch.meshgrid(
-                torch.linspace(0.5, H_ - 0.5, H_, dtype=torch.float32, device=device),
-                torch.linspace(0.5, W_ - 0.5, W_, dtype=torch.float32, device=device),
+                torch.linspace(0.5, shape[0] - 0.5, shape[0], dtype=torch.float32, device=device),
+                torch.linspace(0.5, shape[1] - 0.5, shape[1], dtype=torch.float32, device=device),
             )
-            ref_y = ref_y.reshape(-1)[None] / (valid_ratios[:, None, lvl, 1] * H_)
-            ref_x = ref_x.reshape(-1)[None] / (valid_ratios[:, None, lvl, 0] * W_)
+            ref_y = ref_y.reshape(-1)[None] / (valid_ratios[:, None, lvl, 1] * shape[0])
+            ref_x = ref_x.reshape(-1)[None] / (valid_ratios[:, None, lvl, 0] * shape[1])
             ref = torch.stack((ref_x, ref_y), -1)
             reference_points_list.append(ref)
 
